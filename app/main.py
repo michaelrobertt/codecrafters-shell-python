191285de2632b9538564082751218483bd7f6880
Michael Robert <michaelro0913@gmail.com>
2025-01-12 17:34:23 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,22 +1,32 @@
 import sys
 
 def main():
+    valid_commands = ["exit 0", "exit", "echo", "type"]  # Lista de comandos válidos
     
-    valid_commands = ["exit 0"]  # Lista de comandos válidos
     while True:  # Mantém o loop para aceitar múltiplas entradas
         sys.stdout.write("$ ")
         sys.stdout.flush()  # Garante que o prompt seja exibido corretamente
-        command = input()
         
-        if command[:5] == 'echo ':
-            word = command.replace("echo ", "")
-            print(word)
-        else:
-            if command in valid_commands:
-                if command == "exit 0":
-                    break
+        input_command = input().strip()  # Remove espaços vazios no início e no fim
+        
+        parted_command = input_command.split()  # Divide a string por espaços
+        if not parted_command:  # Ignora entrada vazia
+            continue
+        
+        cmd = parted_command[0]
+        args = parted_command[1:]  # Lista com todos os argumentos
+        
+        if cmd == "echo":
+            print(" ".join(args))  # Imprime todos os argumentos como uma string
+        elif cmd == "type":
+            if args and args[0] in valid_commands: # Verifica se args não está vazio e dps acessa o args 0
+                print(f"{args[0]} is a shell builtin")
             else:
-                print(f"{command}: command not found")
-    
+                print(f"{args[0]}: not found")
+        elif cmd == "exit" or cmd == "exit 0" and not args:
+                break
+        else:
+            print(f"{cmd}: not found")
+
 if __name__ == "__main__":
     main()
