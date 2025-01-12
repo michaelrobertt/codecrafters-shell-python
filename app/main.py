e8e269789cd285acde482d0c675677ec797559a8
Michael Robert <michaelro0913@gmail.com>
2025-01-12 14:39:35 -0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,19 +1,22 @@
 import sys
 
 def main():
-    valid_commands = ["mkdir", "exit 0"]  # Lista de comandos válidos
+    
+    valid_commands = ["exit 0"]  # Lista de comandos válidos
     while True:  # Mantém o loop para aceitar múltiplas entradas
         sys.stdout.write("$ ")
         sys.stdout.flush()  # Garante que o prompt seja exibido corretamente
         command = input()
         
-        if command in valid_commands:
-            if command == "mkdir":
-                print("mkdir: comando executado (simulação)")  # Exemplo de execução
-            elif command == "exit 0":
-                break  # Encerra o loop
+        if command[:5] == 'echo ':
+            word = command.replace("echo ", "")
+            print(word)
         else:
-            print(f"{command}: command not found")
+            if command in valid_commands:
+                if command == "exit 0":
+                    break
+            else:
+                print(f"{command}: command not found")
     
 if __name__ == "__main__":
     main()
